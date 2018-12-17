ae62ed412d599df3ce1203f4aaff38538033d9cd
Marc Francis Cruz <mfgcruz@gmail.com>
2018-12-17 18:11:45 +0800

    cancan rails admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,10 @@ class Ability
       can :view_dashboard, :admin
       can :manage, User
     elsif user.super_admin?    
-      can :manage, :rails_admin
+      can :access, :rails_admin
+      can :dashboard
+      can :manage, :all
+      cannot :view_dashboard, :member
     else
       can :view_dashboard, :member
     end
